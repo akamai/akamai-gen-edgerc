6063097096170730398d2b7273d5d2c8fdb0c566
Kyle Tyacke <ktyacke@gmail.com>
2016-01-25 17:15:13 -0500

    added test to validate that the usage guide is printing correctly when using the -h or --help argument

--- test/gen-edgerc-test.js
+++ test/gen-edgerc-test.js
@@ -21,11 +21,43 @@ var genEdgerc = require('../bin/gen-edgerc');
 var mocha = require('mocha');
 var chai = require('chai');
 var assert = require('chai').assert;
+var child = require('child_process');
+var path = require('path');
+var fs = require('fs-extra');
+var concat = require('concat-stream');
 
-describe('gen-edgerc-test', function() {
+var usage;
+
+var exec;
+
+describe('gen-edgerc', function() {
 
   before(function() {
-
+    exec = path.join(__dirname, "..", "bin", "gen-edgerc");
   });
 
+  it("Outputs a usage guide when the --help argument is passed.", function() {
+    var proc = child.spawn(exec, ["--help"]);
+
+    proc.stdout.pipe(concat(function(output) {
+      var d = output.toString('utf8');
+      var u = fs.readFileSync(path.join(__dirname, "data/usage.txt"), "utf-8");
+      assert.equal(d, u);
+    }));
+  });
+
+  it("Outputs a usage guide when the -h argument is passed.", function() {
+    var proc = child.spawn(exec, ["-h"]);
+
+    proc.stdout.pipe(concat(function(output) {
+      var d = output.toString('utf8');
+      var u = fs.readFileSync(path.join(__dirname, "data/usage.txt"), "utf-8");
+      assert.equal(d, u);
+    }));
+  });
+
+  // TODO: Write tests to cover remaining arguments
+  // -f, --file string      Full path to the credentials file.                           
+  // -s, --section string   Title of the section that will be added to the .edgerc file. 
+  // -p, --path string      Full path to the .edgerc file.  
 });
